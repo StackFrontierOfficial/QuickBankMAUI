894e3383cba56718a703c59740098a01c1a6d143
code@stackfrontier.com <code@stackfrontier.com>
2024-03-15 00:52:40 +0530

    Initial Push

---
+++ QuickBankMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace QuickBankMAUI
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
